2aafd8293e58591efca5d8ee2ac9997334a104d1
Aradhya Srivastava <aradhya610@gmail.com>
2024-08-22 20:31:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('aradhyadotcom')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>please login from chai and</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Chai🍵 aur Code💻</h2>')
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${process.env.PORT}`) 
+})
